d035e0c51f3822e4077f7c5b7f33d04c49a77fc4
coder <soumyodipsutra@gmail.com>
2023-03-18 14:11:04 +0530

    some changes

--- app/src/main/java/com/example/dailynews/CategoryRVAdapter.java
+++ app/src/main/java/com/example/dailynews/CategoryRVAdapter.java
@@ -1,42 +1,66 @@
 package com.example.dailynews;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class CategoryRVAdapter extends RecyclerView.Adapter<CategoryRVAdapter.ViewHolder> {
  private ArrayList<CategoryRVModal>categoryRVModals;
  private Context context;
+ private CategoryClickInterface CategoryClickInterface;
 
-    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModals, Context context) {
+    public CategoryRVAdapter(ArrayList<CategoryRVModal> categoryRVModals, Context context, CategoryRVAdapter.CategoryClickInterface categoryClickInterface) {
         this.categoryRVModals = categoryRVModals;
         this.context = context;
+        CategoryClickInterface = categoryClickInterface;
     }
 
     @NonNull
     @Override
     public CategoryRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.catagories_rv_item,parent,false);
+        return new CategoryRVAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CategoryRVAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull CategoryRVAdapter.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        CategoryRVModal categoryRVModal=categoryRVModals.get(position);
+        holder.categoryTV.setText(categoryRVModal.getCategory());
+        Picasso.get().load(categoryRVModal.getCategory()).into(holder.categoryIV);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CategoryClickInterface.onCategoryClick(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return categoryRVModals.size();
+    }
+    public interface CategoryClickInterface{
+        void onCategoryClick(int position);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView categoryTV;
+        private ImageView categoryIV;
         public ViewHolder(@NonNull View itemView){
             super(itemView);
+            categoryTV = itemView.findViewById(R.id.idTVCategory);
+            categoryIV = itemView.findViewById(R.id.idIVCategory);
         }
     }
 }
--- app/src/main/java/com/example/dailynews/MainActivity.java
+++ app/src/main/java/com/example/dailynews/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.dailynews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CategoryRVAdapter.CategoryClickInterface {
 
     //api key-b3a51abb898d41ac8584930d4f30928e
+
+    private RecyclerView newsRV,categoryRV;
+    private ProgressBar loadingPB;
+    private ArrayList<Articles> articlesArrayList;
+    private ArrayList<CategoryRVModal> categoryRVModalArrayList;
+    private CategoryRVAdapter categoryRVAdapter;
+    private NewsRVAdapter newsRVAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newsRV=findViewById(R.id.idRVNews);
+        categoryRV=findViewById(R.id.idRVCategories);
+        loadingPB=findViewById(R.id.idPBLoading);
+        articlesArrayList=new ArrayList<>();
+        categoryRVModalArrayList=new ArrayList<>();
+        newsRVAdapter = new NewsRVAdapter(articlesArrayList,this);
+        categoryRVAdapter = new CategoryRVAdapter(categoryRVModalArrayList,this,this::onCategoryClick);
+
+    }
+
+    @Override
+    public void onCategoryClick(int position) {
+
     }
 }
